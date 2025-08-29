b24b607ff142efa5d31433766cd8de7c20139531
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-08-29 18:01:56 +0500

    sss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,6 +43,7 @@ const Header = () => {
           <p className=' text-center text-[16px] text-[#FFFFFF] font-extralight w-[38%]'>Welcome to Local Face Perfumes, where the spirit of victory and triumph come alive through scents that empower and inspire. Our curated collection, aptly named "Victory Scented," is a celebration of success and elegance, designed to unleash your victorious essence. Indulge in the sweet taste of triumph with captivating fragrances that tell the tale of your achievements. At Local Face, we believe that every victory deserves a signature scent, and we are dedicated to providing unforgettable fragrances that elevate your spirit and empower your journey.</p>
         </div>
       </main>
+      
       <section className=' h-[100vh]'>
         <div className=' flex '>
           <img className=' w-[40%]' src={Lemon} alt="" />
